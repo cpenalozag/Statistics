2140c1429a58c6842b85a3b462b8bbed09bfd9e8
Carlos Penaloza <c.penaloza@uniandes.edu.co>
2018-04-18 09:43:27 -0500

    Multiple linear regression done

--- T4/Multiple Linear Regression.R
+++ T4/Multiple Linear Regression.R
@@ -19,6 +19,19 @@ Data$Historico<-relevel(Data$Historico,ref="No") #Declare base category
 reducedModel<-lm(Ventas~Historico*`Numero de piezas`+Precio*Dificultad,data=Data)
 summary(reducedModel)
 
+library(car)
+# Partial f for d)
+linearHypothesis(reducedModel, c("HistoricoSi=0", "HistoricoSi:`Numero de piezas`=0"), test="F")
+
+# Partial f for e)
+linearHypothesis(reducedModel, c("DificultadBaja=0", "Precio:DificultadBaja=0"), test="F")
+
+# Partial f for f)
+Data$Historico<-relevel(Data$Historico,ref="No") #Declare base category
+Data$Dificultad<-relevel(Data$Dificultad,ref="Baja") #Declare base category
+linearHypothesis(reducedModel, c("HistoricoSi=0", "HistoricoSi:`Numero de piezas`=0", "DificultadExtrema=0", "Precio:DificultadExtrema=0"), test="F")
+
+
 
 # Model Comparison
 anova(model,reducedModel)
@@ -28,3 +41,19 @@ Data$Dificultad<-relevel(Data$Dificultad,ref="Baja") #Declare base category
 reducedModel<-lm(Ventas~Historico*`Numero de piezas`+Precio*Dificultad,data=Data)
 summary(reducedModel)
 
+#Opción1
+op1<-matrix(c(0,1,190,3.05,0,0,190,0,0),1)
+#Opción2 
+op2<-matrix(c(0,0,220,2.20,0,1,0,0,2.20),1)
+#Comparación
+K<-op1-op2
+K
+K2<-op2-op1
+K2
+library(multcomp)
+#EP
+t<-glht(reducedModel,linfct=K)
+summary(t)
+#EP 2
+t2<-glht(reducedModel,linfct=K2)
+summary(t2)
